2707df2b1469dbb65f9de736f6eb339a0a88014b
Brittany Tinnin <bntinnin7@gmail.com>
2018-07-08 02:19:48 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -6,8 +6,9 @@ class Project
     @backers = []
   end
 
-  def add_backer(name)
-    @backers << name
+  def add_backer(backer)
+    @backers << backer
+    # backer.back_project(self)
   end
 
 end
